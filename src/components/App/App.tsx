99d9ad0be10604fddc6351f53d4e78f4a846fca1
Vadim Belousov <thevadiratti@yandex.ru>
2022-08-04 14:12:30 +0500

    feat: добавить header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import AppHeader from '../AppHeader/AppHeader';
-import appStyles from './app.module.css';
+import appStyles from './App.module.css';
 
 class App extends React.Component {
   render() {
     return <div className={appStyles.app}>
-      
+      <AppHeader />
     </div>
   }
 }
--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import headerStyles from './AppHeader.module.css';
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 class AppHeader extends React.Component {
   render() {
     return <header className={headerStyles.header}>
-      <Logo />
+
+      <div className={`${headerStyles.container} mt-4 mb-4`}>
+        <menu className={headerStyles.menu}>
+          <li className={`${headerStyles.item} p-5`}>
+            <BurgerIcon type="primary" />
+            <span className='text text_type_main-default ml-2'>Конструктор</span>
+          </li>
+          <li className={`${headerStyles.item} p-5 ml-2`}>
+            <ListIcon type="secondary" />
+            <span className='text text_type_main-default text_color_inactive ml-2'>Лента заказов</span>
+          </li>
+        </menu>
+
+        <Logo />
+
+        <div className={`${headerStyles.profile} p-5`}>
+          <ProfileIcon type='secondary' />
+          <span className='text text_type_main-default text_color_inactive ml-2'>Личный кабинет</span>
+        </div>
+      </div>
     </header>
   }
 }
