d377f1b3355147195de767b3ae5f7b5976f172f7
Vadim Belousov <thevadiratti@yandex.ru>
2022-08-15 13:35:59 +0500

    feat: добавить компонент OrderDetails

--- src/components/ModalOverlay/ModalOverlay.js
+++ src/components/ModalOverlay/ModalOverlay.js
@@ -8,8 +8,16 @@ function ModalOverlay(props) {
   return ReactDOM.createPortal(
     (
       <div className={modalOverlayStyle.overlay}>
-        <div className={modalOverlayStyle.window}>
-          <div className={modalOverlayStyle.icon}><CloseIcon type='primary' /></div>
+        <div className={`pt-10 pb-30 ${modalOverlayStyle.window}`}>
+          <div className={modalOverlayStyle.cnt}>
+            <p2 className="pt-8 text text_type_main-large"></p2>
+            <div className={modalOverlayStyle.closeIcon}><CloseIcon type='primary' /></div>
+          </div>
+          <span className='pt-7 text text_type_digits-large'>034563</span>
+          <span className='text text_type_main-medium mt-8'>идентификатор заказа</span>
+          <div className={`mt-15 ${modalOverlayStyle.doneIcon}`}></div>
+          <span className='text text_type_main-default mt-15'>Ваш заказ начали готовить</span>
+          <span className='text text_type_main-default text_color_inactive mt-2'>Дождитесь готовности на орбитальной станции</span>
           {props.children}
         </div>
       </div>
---
+++ src/components/OrderDetails/OrderDetails.js
@@ -0,0 +1,15 @@
+import orderDetailsStyles from './OrderDetails.module.css';
+
+function OrderDetails() {
+  return (
+    <>
+      <span className='text text_type_digits-large'>{Math.floor(Math.random( ) * (999999 + 1))}</span>
+      <span className='text text_type_main-medium mb-8'>идентификатор заказа</span>
+      <div className={`mt-15 ${orderDetailsStyles.doneIcon}`}></div>
+      <span className='text text_type_main-default mt-15'>Ваш заказ начали готовить</span>
+      <span className='text text_type_main-default text_color_inactive mt-2'>Дождитесь готовности на орбитальной станции</span>
+    </>
+  )
+}
+
+export default OrderDetails;
