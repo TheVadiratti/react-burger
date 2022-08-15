3dc404f702d5c4cffd116525c472c17eaaf2fc55
Vadim Belousov <thevadiratti@yandex.ru>
2022-08-15 13:37:07 +0500

    fix: поправить верстку OrderDetails

--- src/components/ModalOverlay/ModalOverlay.js
+++ src/components/ModalOverlay/ModalOverlay.js
@@ -14,7 +14,7 @@ function ModalOverlay(props) {
             <div className={modalOverlayStyle.closeIcon}><CloseIcon type='primary' /></div>
           </div>
           <span className='pt-7 text text_type_digits-large'>034563</span>
-          <span className='text text_type_main-medium mt-8'>идентификатор заказа</span>
+          <span className='text text_type_main-medium mt-9'>идентификатор заказа</span>
           <div className={`mt-15 ${modalOverlayStyle.doneIcon}`}></div>
           <span className='text text_type_main-default mt-15'>Ваш заказ начали готовить</span>
           <span className='text text_type_main-default text_color_inactive mt-2'>Дождитесь готовности на орбитальной станции</span>
