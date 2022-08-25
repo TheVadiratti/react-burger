4dc43a58006f998a45b93390bfc2b58de2b7946d
Vadim Belousov <thevadiratti@yandex.ru>
2022-08-25 11:37:59 +0500

    feat: добавить в конструктор бургера данные с api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,11 +5,13 @@ import Main from '../Main/Main';
 import Modal from '../Modal/Modal';
 import IngredientDetails from '../IngredientDetails/IngredientDetails';
 import OrderDetails from '../OrderDetails/OrderDetails';
+import { ConstructorContext } from '../../services/ConstructorContext';
 
 function App() {
   const [data, setData] = React.useState([]);
   const [onPopup, setOnPopup] = React.useState({ open: false, type: '' });
   const [selectedIngredient, setSelectedIngredient] = React.useState({});
+  const [constructorData, setConstructorData] = React.useState({bun: {}, main: []});
   const baseUrl = 'https://norma.nomoreparties.space/api/ingredients/';
 
   React.useEffect(() => {
@@ -21,7 +23,19 @@ function App() {
         return Promise.reject(`Ошибка: ${res.status}`);
       })
       .then(res => {
-        setData(res.data)
+        setData(res.data);
+
+        // данные с api для первой проверки
+        const bun = res.data.find(item => {
+          if(item.type === 'bun') {
+            return item;
+          }
+        })
+        const main = res.data.filter(item => {
+            return item.type !== 'bun';
+          }
+        )
+        setConstructorData({bun: bun, main: main});
       })
       .catch(error => {
         console.log(error);
@@ -31,11 +45,13 @@ function App() {
   return (
     <div className={appStyles.app}>
       <AppHeader />
-      <Main
-        data={data}
-        setOnPopup={setOnPopup}
-        setSelectedIngredient={setSelectedIngredient}
-      />
+      <ConstructorContext.Provider value={constructorData}>
+        <Main
+          data={data}
+          setOnPopup={setOnPopup}
+          setSelectedIngredient={setSelectedIngredient}
+        />
+      </ConstructorContext.Provider>
       {onPopup.open && (
         <Modal heading={onPopup.type === 'IngredientDetails' ? 'Детали ингредиента' : ''} setOnPopup={setOnPopup}>
           {onPopup.type === 'IngredientDetails' && (
--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import burgerConstructorStyles from './BurgerConstructor.module.css';
 import { ConstructorElement, Button, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
+import { ConstructorContext } from '../../services/ConstructorContext';
 
 function BurgerConstructor(props) {
+  const ingredientData = React.useContext(ConstructorContext);
+
   function openOrderDetailsPopup() {
     props.setOnPopup({
       open: true,
@@ -10,65 +14,37 @@ function BurgerConstructor(props) {
     })
   }
 
+  function createIngredient(ingredient, type, isLocked, isMain, text, key) {
+    return (
+      <div className={isMain ? burgerConstructorStyles.item : burgerConstructorStyles.itemLocked} key={key}>
+        {isMain && <DragIcon type='primary' />}
+        <ConstructorElement
+          type={isMain ? '' : type}
+          isLocked={isLocked}
+          text={`${ingredient.name} ${text}`}
+          price={ingredient.price}
+          thumbnail={ingredient.image}
+        />
+      </div> 
+    )
+  }
+
   return (
     <section className={burgerConstructorStyles.section}>
       <div className={burgerConstructorStyles.ingredients}>
-        <div className={burgerConstructorStyles.itemLocked}>
-          <ConstructorElement
-            type="top"
-            isLocked={true}
-            text="Краторная булка N-200i (верх)"
-            price={200}
-            thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-          />
-        </div>
+
+        {createIngredient(ingredientData.bun, 'top', true, false, '(верх)', 1)}
+
         <div className={`mt-4 mb-4 ${burgerConstructorStyles.window}`}>
-          <div className={burgerConstructorStyles.item}>
-            <DragIcon type='primary' />
-            <ConstructorElement
-              isLocked={false}
-              text="Сыр с астероидной плесенью"
-              price={4142}
-              thumbnail={'https://code.s3.yandex.net/react/code/cheese.png'}
-            />
-          </div>
-          <div className={burgerConstructorStyles.item}>
-            <DragIcon type='primary' />
-            <ConstructorElement
-              isLocked={false}
-              text="Мини-салат Экзо-Плантаго"
-              price={4400}
-              thumbnail={'https://code.s3.yandex.net/react/code/salad.png'}
-            />
-          </div>
-          <div className={burgerConstructorStyles.item}>
-            <DragIcon type='primary' />
-            <ConstructorElement
-              isLocked={false}
-              text="Соус фирменный Space Sauce"
-              price={80}
-              thumbnail={'https://code.s3.yandex.net/react/code/sauce-04.png'}
-            />
-          </div>
-          <div className={burgerConstructorStyles.item}>
-            <DragIcon type='primary' />
-            <ConstructorElement
-              isLocked={false}
-              text="Филе Люминесцентного тетраодонтимформа"
-              price={988}
-              thumbnail={'https://code.s3.yandex.net/react/code/meat-03.png'}
-            />
-          </div>
+
+          {ingredientData.main.map(item => {
+            return createIngredient(item, false, false, true, '', item._id);
+          })}
+
         </div>
-        <div className={burgerConstructorStyles.itemLocked}>
-          <ConstructorElement
-            type="bottom"
-            isLocked={true}
-            text="Краторная булка N-200i (низ)"
-            price={200}
-            thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-          />
-        </div>
+        
+        {createIngredient(ingredientData.bun, 'bottom', true, false, '(низ)', 2)}
+
       </div>
       <div className={`${burgerConstructorStyles.total} mt-10 pr-4`}>
         <Button type="primary" size="large" onClick={openOrderDetailsPopup}>
