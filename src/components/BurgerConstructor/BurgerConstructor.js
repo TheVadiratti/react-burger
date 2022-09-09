666d9064799007e45001af844d584b9d5037f758
Vadim Belousov <thevadiratti@yandex.ru>
2022-09-09 14:36:22 +0500

    fix: убрать лишний код из компонента конструктора

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import burgerConstructorStyles from './BurgerConstructor.module.css';
 import { ConstructorElement, Button, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
@@ -10,7 +9,6 @@ import { useDrop } from 'react-dnd/dist/hooks/useDrop';
 function BurgerConstructor() {
   const ingredientsData = useSelector((state) => state.ingredients);
   const constructorStructure = useSelector((state) => state.constructor);
-  const [state, setState] = React.useState(null);
   const dispatch = useDispatch();
 
   const [, dropTarget] = useDrop({
@@ -25,27 +23,6 @@ function BurgerConstructor() {
       });
     }
   });
-
-  React.useEffect(() => {
-    const loadData = new Promise(function (resolve) {
-      if (ingredientsData.length !== 0) {
-        resolve();
-      }
-    });
-
-    loadData.then(() => {
-      const bun = ingredientsData.find(item => {
-        if (item.type === 'bun') {
-          return item;
-        }
-      })
-      const main = ingredientsData.filter(item => {
-        return item.type !== 'bun';
-      })
-      setState({ bun: bun, main: main });
-    })
-    console.log(constructorStructure);
-  }, [ingredientsData, constructorStructure]);
 
   function createIngredient(ingredient, type, isLocked, isMain, text, key) {
     return (
@@ -74,9 +51,9 @@ function BurgerConstructor() {
   }
 
   function sendOrder() {
-    const orderList = Object.assign([], state.main);
-    orderList.unshift(state.bun);
-    orderList.push(state.bun);
+    const orderList = Object.assign([], constructorStructure.main);
+    orderList.unshift(constructorStructure.buns);
+    orderList.push(constructorStructure.buns);
     fetch(`${baseUrl}/api/orders/`, {
       method: 'POST',
       headers: {
@@ -96,38 +73,36 @@ function BurgerConstructor() {
   }
 
   return (
-    state && (
-      <section className={burgerConstructorStyles.section}>
-        <div ref={dropTarget} className={burgerConstructorStyles.ingredients}>
+    <section className={burgerConstructorStyles.section}>
+      <div ref={dropTarget} className={burgerConstructorStyles.ingredients}>
 
-          {constructorStructure.buns.name ? createIngredient(constructorStructure.buns, 'top', true, false, '(верх)', 1) : <p className={`${burgerConstructorStyles.instruction} text text_type_digits-default`}>Добавьте булки</p>}
+        {constructorStructure.buns.name ? createIngredient(constructorStructure.buns, 'top', true, false, '(верх)', 1) : <p className={`${burgerConstructorStyles.instruction} text text_type_digits-default`}>Добавьте булки</p>}
 
-          <div className={`mt-4 mb-4 ${burgerConstructorStyles.window}`}>
+        <div className={`mt-4 mb-4 ${burgerConstructorStyles.window}`}>
 
-            {constructorStructure.main.length !== 0 ?
-              constructorStructure.main.map(item => {
-                return createIngredient(item, null, false, true, '', item._id);
-              })
-              :
-              <p className={`${burgerConstructorStyles.instruction} text text_type_digits-default`}>Добавьте ингредиенты</p>
-              }
-
-          </div>
-
-          {constructorStructure.buns.name && createIngredient(constructorStructure.buns, 'bottom', true, false, '(низ)', 2)}
+          {constructorStructure.main.length !== 0 ?
+            constructorStructure.main.map(item => {
+              return createIngredient(item, null, false, true, '', item._id);
+            })
+            :
+            <p className={`${burgerConstructorStyles.instruction} text text_type_digits-default`}>Добавьте ингредиенты</p>
+          }
 
         </div>
-        <div className={`${burgerConstructorStyles.total} mt-10 pr-4`}>
-          <Button type="primary" size="large" onClick={sendOrder}>
-            Оформить заказ
-          </Button>
-          <div className={`${burgerConstructorStyles.sum} mr-10`}>
-            <p className={`${burgerConstructorStyles.digit} text text_type_digits-medium mr-2`}>{`${sum()}`}</p>
-            <CurrencyIcon type="primary" />
-          </div>
+
+        {constructorStructure.buns.name && createIngredient(constructorStructure.buns, 'bottom', true, false, '(низ)', 2)}
+
+      </div>
+      <div className={`${burgerConstructorStyles.total} mt-10 pr-4`}>
+        <Button type="primary" size="large" onClick={sendOrder}>
+          Оформить заказ
+        </Button>
+        <div className={`${burgerConstructorStyles.sum} mr-10`}>
+          <p className={`${burgerConstructorStyles.digit} text text_type_digits-medium mr-2`}>{`${sum()}`}</p>
+          <CurrencyIcon type="primary" />
         </div>
-      </section>
-    )
+      </div>
+    </section>
   )
 }
 
