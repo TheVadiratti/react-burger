e2c73abd835c40cd4387d561da5814d5cdbc1999
Vadim Belousov <thevadiratti@yandex.ru>
2022-09-29 12:35:09 +0500

    fix: удалить лишние компоненты

--- src/components/AuthorizationInputs/BaseInput.js
+++
@@ -1,9 +0,0 @@
-import baseInputStyles from './AuthorizationInputs.module.css';
-
-function BaseInput({type, placeholder, margin}) {
-  return (
-    <input type={type} className={`text text_type_main-default text_color_inactive ${margin} pl-6 ${baseInputStyles.input}`} placeholder={placeholder}></input>
-  )
-}
-
-export default BaseInput;
