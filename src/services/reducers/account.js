ca510037c3dd3e3835933e853563dff721ada2ca
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-01 17:40:21 +0500

    refactor: разделить редьюсеры по файлам

---
+++ src/services/reducers/account.js
@@ -0,0 +1,154 @@
+import {
+  CHANGE_PASSWORD_REQUEST,
+  CHANGE_PASSWORD_SUCCESS,
+  CHANGE_PASSWORD_ERROR,
+  RESET_PASSWORD_REQUEST,
+  RESET_PASSWORD_SUCCESS,
+  RESET_PASSWORD_ERROR,
+  REGISTRATION_REQUEST,
+  REGISTRATION_SUCCESS,
+  REGISTRATION_ERROR
+} from "../../utils/constants";
+
+const changePasswordState = {
+  isLoaded: false,
+  isSuccess: false,
+  isError: false,
+  message: null
+}
+
+const resetPasswordState = {
+  isLoaded: false,
+  isSuccess: false,
+  isError: false,
+  message: null
+}
+
+const registrationState = {
+  isLoaded: false,
+  isSuccess: false,
+  isError: false
+}
+
+const changePassword = (state = changePasswordState, action) => {
+  switch(action.type) {
+    case CHANGE_PASSWORD_REQUEST:
+      return {
+        ...state,
+        isLoaded: true,
+        isSuccess: false,
+        isError: false
+      }
+    
+    case CHANGE_PASSWORD_SUCCESS:
+      if(action.result) {
+        return {
+          isLoaded: false,
+          isSuccess: true,
+          isError: false,
+          message: action.message
+        }
+      }
+      else {
+        return {
+          isLoaded: false,
+          isSuccess: false,
+          isError: true,
+          message: action.message
+        }
+      }
+
+    case CHANGE_PASSWORD_ERROR:
+      return {
+        ...state,
+        isLoaded: false,
+        isSuccess: false,
+        isError: true
+      }
+      
+    default:
+      return state;
+  }
+}
+
+const resetPassword = (state = resetPasswordState, action) => {
+  switch(action.type) {
+    case RESET_PASSWORD_REQUEST:
+      return {
+        ...state,
+        isLoaded: true,
+        isSuccess: false,
+        isError: false
+      }
+    
+    case RESET_PASSWORD_SUCCESS:
+      if(action.result) {
+        return {
+          isLoaded: false,
+          isSuccess: true,
+          isError: false,
+          message: action.message
+        }
+      }
+      else {
+        return {
+          isLoaded: false,
+          isSuccess: false,
+          isError: true,
+          message: action.message
+        }
+      }
+
+    case RESET_PASSWORD_ERROR:
+      return {
+        ...state,
+        isLoaded: false,
+        isSuccess: false,
+        isError: true
+      }
+      
+    default:
+      return state;
+  }
+}
+
+const registration = (state = registrationState, action) => {
+  switch(action.type) {
+    case REGISTRATION_REQUEST:
+      return {
+        ...state,
+        isLoaded: true,
+        isSuccess: false,
+        isError: false
+      }
+    
+    case REGISTRATION_SUCCESS:
+      if(action.result) {
+        return {
+          isLoaded: false,
+          isSuccess: true,
+          isError: false
+        }
+      }
+      else {
+        return {
+          isLoaded: false,
+          isSuccess: false,
+          isError: true
+        }
+      }
+
+    case REGISTRATION_ERROR:
+      return {
+        ...state,
+        isLoaded: false,
+        isSuccess: false,
+        isError: true
+      }
+      
+    default:
+      return state;
+  }
+}
+
+export { changePassword, resetPassword, registration };
---
+++ src/services/reducers/modal.js
@@ -0,0 +1,38 @@
+import {
+  CLOSE_MODAL,
+  OPEN_INGREDIENT_DETAILS,
+  OPEN_ORDER_DETAILS
+} from '../../utils/constants';
+
+const modalState = {
+  open: false,
+  type: '',
+  dataIngredientDetails: {}
+}
+
+const modal = (state = modalState, action) => {
+  switch (action.type) {
+    case OPEN_INGREDIENT_DETAILS:
+      return {
+        ...state,
+        open: true,
+        type: 'IngredientDetails',
+        dataIngredientDetails: action.data
+      }
+
+    case OPEN_ORDER_DETAILS:
+      return {
+        ...state,
+        open: true,
+        type: 'OrderDetails'
+      }
+
+    case CLOSE_MODAL:
+      return modalState
+
+    default:
+      return state;
+  }
+}
+
+export { modal };
