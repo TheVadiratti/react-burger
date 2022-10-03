b8341e452c58ffd6b5b7528c7e1a2db6018b0cb1
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-03 12:48:35 +0500

    fix: доработать AppHeader

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,21 +1,23 @@
 import headerStyles from './AppHeader.module.css';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 function AppHeader() {
+  const { pathname } = useLocation();
+
   return (
     <header className={headerStyles.header}>
       <div className={`${headerStyles.container} mt-3 mb-4`}>
         <menu className={headerStyles.menu}>
           <li className={`${headerStyles.item} p-5`}>
             <NavLink to='/' className={headerStyles.link} activeClassName={headerStyles.link_active} exact={true}>
-              <BurgerIcon type="secondary" />
+              <BurgerIcon type={pathname === '/' ? 'primary' : 'secondary'} />
               <span className='text text_type_main-default ml-2'>Конструктор</span>
             </NavLink>
           </li>
           <li className={`${headerStyles.item} p-5 ml-2`}>
             <NavLink to='/orders' className={headerStyles.link} activeClassName={headerStyles.link_active} exact={true}>
-              <ListIcon type="secondary" />
+              <ListIcon type={pathname === '/orders' ? 'primary' : 'secondary'} />
               <span className='text text_type_main-default ml-2'>Лента заказов</span>
             </NavLink>
           </li>
@@ -25,7 +27,7 @@ function AppHeader() {
 
         <NavLink to='/profile' className={headerStyles.link} activeClassName={headerStyles.link_active} exact={true}>
           <div className={`${headerStyles.profile} p-5`}>
-            <ProfileIcon type='secondary' />
+            <ProfileIcon type={pathname === '/profile' ? 'primary' : 'secondary'} />
             <span className='text text_type_main-default ml-2'>Личный кабинет</span>
           </div>
         </NavLink>
