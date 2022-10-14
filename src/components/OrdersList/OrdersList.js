cd132cf7ebe751e6382cece5650702460521cfb1
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-14 16:42:00 +0500

    feat: доработка /profile/orders

--- src/components/OrdersList/OrdersList.js
+++ src/components/OrdersList/OrdersList.js
@@ -6,14 +6,14 @@ function OrdersList() {
     <section className={`${ordersListStyles.window}`}>
       <div className={`p-6 ${ordersListStyles.item}`}>
         <div className={ordersListStyles.info}>
-          <span className='text text_type_digits-default'>#034535</span>
-          <span className='text text_type_main-default text_color_inactive'>Сегодня, 16:20 i-GMT+3</span>
+          <span className="text text_type_digits-default">#034535</span>
+          <span className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</span>
         </div>
-        <h4 className='text text_type_main-medium'>Death Star Starship Main бургер</h4>
+        <h4 className="text text_type_main-medium">Death Star Starship Main бургер</h4>
         <div className={ordersListStyles.componentsCnt}>
           <div className={ordersListStyles.components}></div>
           <div className={ordersListStyles.sum}>
-            <span className='text text_type_digits-default'>480</span>
+            <span className="text text_type_digits-default">480</span>
             <CurrencyIcon type="primary" />
           </div>
         </div>
--- src/components/ProfileHistory/ProfileHistory.js
+++ src/components/ProfileHistory/ProfileHistory.js
@@ -5,7 +5,10 @@ function ProfileHistory() {
   return (
     <ul className={profileHistoryStyles.orders}>
       <li className={`p-6 ${profileHistoryStyles.order}`}>
-        <span className="text text_type_digits-default">#034535</span>
+        <div className={profileHistoryStyles.idCnt}>
+          <span className="text text_type_digits-default">#034535</span>
+          <span className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</span>
+        </div>
         <h3 className="text text_type_main-medium mt-6">Death Star Starship Main бургер</h3>
         <span className="text text_type_main-default mt-2">Создан</span>
         <div className={`mt-6 ${profileHistoryStyles.cnt}`}>
