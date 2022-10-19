9b01839c15bb21443bd96105db15c0ce78ae2150
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-19 19:40:39 +0500

    feat: добавить отображение ингредиентов в карточках заказов

--- src/components/OrdersList/OrdersList.js
+++ src/components/OrdersList/OrdersList.js
@@ -5,8 +5,11 @@ import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components
 import { getTimeString } from '../../utils/utils';
 
 function OrdersList() {
+  const ingredients = useSelector((state) => state.ingredients.data);
   const orders = useSelector((state) => state.orders);
   const windowCntRef = useRef(null);
+
+  console.log(ingredients);
 
   function renderOrderCard() {
     return orders.list.map((order, i) => {
@@ -18,12 +21,27 @@ function OrdersList() {
           </div>
           <h4 className="text text_type_main-medium">{order.name}</h4>
           <div className={ordersListStyles.componentsCnt}>
-            <div className={ordersListStyles.components}></div>
+            <div className={ordersListStyles.components}>
+              {renderOrderComponents(order.ingredients)}
+            </div>
             <div className={ordersListStyles.sum}>
               <span className="text text_type_digits-default">480</span>
               <CurrencyIcon type="primary" />
             </div>
           </div>
+        </div>
+      )
+    })
+  }
+
+  function renderOrderComponents(ingredientsArr) {
+    return ingredientsArr.map((ingredientID, i) => {
+      const currentIngredient = ingredients.find(item => {
+        return item._id === ingredientID;
+      })
+      return (
+        <div className={ordersListStyles.fakeBorder} style={{zIndex: `${ingredientsArr.length - i}`}}>
+          <div className={ordersListStyles.cardImg} style={{ backgroundImage: `url(${currentIngredient.image})` }} key={i}></div>
         </div>
       )
     })
