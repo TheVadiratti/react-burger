aea4354ec902726d2ecdb1d0edd7a8365126ba88
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-19 16:30:23 +0500

    feat: добавить функцию рендера времени на карточке заказа

--- src/components/OrdersList/OrdersList.js
+++ src/components/OrdersList/OrdersList.js
@@ -1,23 +1,37 @@
+import { useRef } from 'react';
 import ordersListStyles from './OrdersList.module.css';
+import { useSelector } from 'react-redux';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { getTimeString } from '../../utils/utils';
 
 function OrdersList() {
-  return (
-    <section className={`${ordersListStyles.window}`}>
-      <div className={`p-6 ${ordersListStyles.item}`}>
-        <div className={ordersListStyles.info}>
-          <span className="text text_type_digits-default">#034535</span>
-          <span className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</span>
-        </div>
-        <h4 className="text text_type_main-medium">Death Star Starship Main бургер</h4>
-        <div className={ordersListStyles.componentsCnt}>
-          <div className={ordersListStyles.components}></div>
-          <div className={ordersListStyles.sum}>
-            <span className="text text_type_digits-default">480</span>
-            <CurrencyIcon type="primary" />
+  const orders = useSelector((state) => state.orders);
+  const windowCntRef = useRef(null);
+
+  function renderOrderCard() {
+    return orders.list.map((order, i) => {
+      return (
+        <div className={`p-6 ${ordersListStyles.item}`} key={i}>
+          <div className={ordersListStyles.info}>
+            <span className="text text_type_digits-default">{'#' + order.number}</span>
+            <span className="text text_type_main-default text_color_inactive">{getTimeString(order.createdAt)}</span>
+          </div>
+          <h4 className="text text_type_main-medium">{order.name}</h4>
+          <div className={ordersListStyles.componentsCnt}>
+            <div className={ordersListStyles.components}></div>
+            <div className={ordersListStyles.sum}>
+              <span className="text text_type_digits-default">480</span>
+              <CurrencyIcon type="primary" />
+            </div>
           </div>
         </div>
-      </div>
+      )
+    })
+  }
+
+  return (
+    <section ref={windowCntRef} className={`${ordersListStyles.window}`}>
+      {renderOrderCard()}
     </section>
   )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 function checkResponse(res) {
-  if(res.ok) {
+  if (res.ok) {
     return res.json();
   }
   else {
@@ -15,4 +15,79 @@ function hidePassword(ref) {
   ref.current.setAttribute('type', 'text');
 }
 
-export { checkResponse, showPassword, hidePassword };
+function getTimeString(ISO) {
+  const date = new Date(ISO);
+  const dateToday = new Date();
+
+  const hours = date.getUTCHours() > 9 ? String(date.getUTCHours()) : '0' + String(date.getUTCHours());
+  const minutes = date.getUTCMinutes() > 9 ? String(date.getUTCMinutes()) : '0' + String(date.getUTCMinutes());
+
+  function getLastDigit(timeframe) {
+    const array = Array.from(timeframe);
+    return array[array.length - 1];
+  }
+
+  let period;
+  if (dateToday.getUTCFullYear() > date.getUTCFullYear()) {
+    period = String(dateToday.getUTCFullYear() - date.getUTCFullYear());
+    const lastDigit = getLastDigit(period);
+    if (lastDigit === 1) {
+      period = period + 'год';
+    }
+    else if (lastDigit > 1 && lastDigit < 5) {
+      period = period + 'года';
+    }
+    else if (lastDigit > 4 && lastDigit < 10) {
+      period = period + 'лет';
+    }
+    period = period + 'назад,';
+  }
+
+  else if (dateToday.getUTCMonth() > date.getUTCMonth()) {
+    period = String(dateToday.getUTCMonth() - date.getUTCMonth());
+    const lastDigit = getLastDigit(period);
+    if (lastDigit === 1) {
+      period = period + 'месяц';
+    }
+    else if (lastDigit > 1 && lastDigit < 5) {
+      period = period + 'месяца';
+    }
+    else if (lastDigit > 4 && lastDigit < 10) {
+      period = period + 'месяцев';
+    }
+    period = period + 'назад';
+  }
+
+  else if (dateToday.getUTCDate() >= date.getUTCDate()) {
+    period = String(dateToday.getUTCDate() - date.getUTCDate());
+    const lastDigit = Number(getLastDigit(period));
+    // me here
+    if (Number(period) < 2) {
+      if (Number(period) === 0) {
+        period = 'Сегодня,';
+      }
+      else if (lastDigit === 1) {
+        period = 'Вчера,';
+      }
+    }
+    else {
+      if (lastDigit > 1 && lastDigit < 5) {
+        period = period + 'дня';
+      }
+      else {
+        period = period + 'дней';
+      }
+      period = period + ' назад';
+    }
+  }
+
+
+  let sign = String(date.getTimezoneOffset()).slice(0, 1);
+  sign = sign === '+' ? '-' : '+';
+  let gmt = String(date.getTimezoneOffset()).slice(1);
+  gmt = sign + Number(gmt) / 60;
+
+  return period + ' ' + hours + ':' + minutes + ' i-GMT' + gmt;
+}
+
+export { checkResponse, showPassword, hidePassword, getTimeString };
