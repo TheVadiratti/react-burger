ea9ad07406e32732a56f24039d8e332a82d34ebc
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-24 18:32:40 +0500

    feat: доделать описания в /profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -15,7 +15,10 @@ function Profile() {
 
   useEffect(() => {
     dispatch(getUserDataFetchAction());
-  }, [])
+  }, []);
+
+  const description = location.pathname === '/profile' ? 'В этом разделе вы можете изменить свои персональные данные' :
+    'В этом разделе вы можете просмотреть свою историю заказов';
 
   function logout() {
     dispatch(logoutFetchAction());
@@ -27,14 +30,11 @@ function Profile() {
         <nav>
           <ul className={`text text_type_main-medium ${profileStyles.links}`}>
             <li className={profileStyles.item}><NavLink to="/profile" className={profileStyles.link} activeStyle={{ color: "#F2F2F3" }} exact>Профиль</NavLink></li>
-            <li className={profileStyles.item}><NavLink to="/profile/orders" className={profileStyles.link} activeStyle={{ color: "#F2F2F3" }} exact>История</NavLink></li>
+            <li className={profileStyles.item}><NavLink to="/profile/orders" className={profileStyles.link} activeStyle={{ color: "#F2F2F3" }} exact>История заказов</NavLink></li>
             <li onClick={logout} className={profileStyles.item}><span className={profileStyles.link}>Выход</span></li>
           </ul>
         </nav>
-        {
-          location.pathname === '/profile' &&
-          <p className={`text text_type_main-default text_color_inactive mt-20`}>В этом разделе вы можете изменить свои персональные данные</p>
-        }
+          <p className={`text text_type_main-default text_color_inactive mt-20`}>{description}</p>
       </section>
       <section className={profileStyles.content}>
         <Switch>
