581094567731f0b8d12fe32ed3b34a210bcd5492
Vadim Belousov <thevadiratti@yandex.ru>
2022-10-29 13:39:45 +0500

    fix: переделать логику работы подсказок в /profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
@@ -12,7 +12,6 @@ import Loader from "../../components/Loader/Loader";
 import { wsMyOrdersActions } from "../../utils/constants";
 
 function Profile() {
-  const [description, setDescription] = useState('');
   const dispatch = useDispatch();
   const location = useLocation();
   const pageView = useSelector((state) => state.modal.pageView);
@@ -27,23 +26,24 @@ function Profile() {
         type: wsMyOrdersActions.start
       });
     }
-    switch (location.pathname) {
-      case '/profile':
-        setDescription('В этом разделе вы можете изменить свои персональные данные');
-        break;
-      case '/profile/orders':
-        setDescription('В этом разделе вы можете просмотреть свою историю заказов');
-        break;
-    }
     return () => {
       dispatch({
         type: wsMyOrdersActions.closed
       });
     }
-  }, [hasToken, location]);
+  }, [hasToken]);
 
   function logout() {
     dispatch(logoutFetchAction());
+  }
+
+  function getDescription () {
+    switch (location.pathname) {
+      case '/profile':
+        return 'В этом разделе вы можете изменить свои персональные данные';
+      case '/profile/orders':
+        return 'В этом разделе вы можете просмотреть свою историю заказов';
+    }
   }
 
   if (isDataLoaded || !data.list || !hasToken) {
@@ -62,7 +62,7 @@ function Profile() {
               <li onClick={logout} className={profileStyles.item}><span className={profileStyles.link}>Выход</span></li>
             </ul>
           </nav>
-          <p className={`text text_type_main-default text_color_inactive mt-20`}>{description}</p>
+          <p className={`text text_type_main-default text_color_inactive mt-20`}>{getDescription ()}</p>
         </section>
         <section className={profileStyles.content}>
           <Switch>
