124132b5c9f67f3183d80b4cdbc948c8f2bf104f
Vadim Belousov <thevadiratti@yandex.ru>
2022-11-13 12:53:19 +0500

    refactor: ts типизация web-socket (кроме middleware)

--- src/services/reducers/web-socket.ts
+++ src/services/reducers/web-socket.ts
@@ -1,11 +1,12 @@
 import { wsAllOrdersActions, wsMyOrdersActions } from '../../utils/constants';
+import { IwsOrdersAction } from '../types/web-socket';
 
 const socketState = {
   wsConnected: false,
   error: undefined
 };
 
-export const ws = (state = socketState, action) => {
+export const ws = (state = socketState, action: IwsOrdersAction) => {
   switch (action.type) {
     case wsAllOrdersActions.success || wsMyOrdersActions.success:
       return {
---
+++ src/services/types/web-socket.ts
@@ -0,0 +1,51 @@
+import {
+  WS_ALL_ORDERS_START,
+  WS_ALL_ORDERS_SUCCESS,
+  WS_ALL_ORDERS_ERROR,
+  WS_ALL_ORDERS_CLOSED,
+  WS_ALL_ORDERS_GET_MESSAGE,
+  WS_ALL_ORDERS_SEND_MESSAGE,
+  WS_MY_ORDERS_START,
+  WS_MY_ORDERS_SUCCESS,
+  WS_MY_ORDERS_ERROR,
+  WS_MY_ORDERS_CLOSED,
+  WS_MY_ORDERS_GET_MESSAGE,
+  WS_MY_ORDERS_SEND_MESSAGE
+} from '../../utils/constants';
+
+export interface IwsAllOrders {
+  start: typeof WS_ALL_ORDERS_START;
+  success: typeof WS_ALL_ORDERS_SUCCESS;
+  error: typeof WS_ALL_ORDERS_ERROR;
+  closed: typeof WS_ALL_ORDERS_CLOSED;
+  getMessage: typeof WS_ALL_ORDERS_GET_MESSAGE;
+  sendMessage: typeof WS_ALL_ORDERS_SEND_MESSAGE;
+}
+
+export interface IwsMyOrders {
+  start: typeof WS_MY_ORDERS_START;
+  success: typeof WS_MY_ORDERS_SUCCESS;
+  error: typeof WS_MY_ORDERS_ERROR;
+  closed: typeof WS_MY_ORDERS_CLOSED;
+  getMessage: typeof WS_MY_ORDERS_GET_MESSAGE;
+  sendMessage: typeof WS_MY_ORDERS_SEND_MESSAGE;
+}
+
+type TwsOrders =
+  & typeof WS_MY_ORDERS_START
+  & typeof WS_MY_ORDERS_SUCCESS
+  & typeof WS_MY_ORDERS_ERROR
+  & typeof WS_MY_ORDERS_CLOSED
+  & typeof WS_MY_ORDERS_GET_MESSAGE
+  & typeof WS_MY_ORDERS_SEND_MESSAGE
+  & typeof WS_MY_ORDERS_START
+  & typeof WS_MY_ORDERS_SUCCESS
+  & typeof WS_MY_ORDERS_ERROR
+  & typeof WS_MY_ORDERS_CLOSED
+  & typeof WS_MY_ORDERS_GET_MESSAGE
+  & typeof WS_MY_ORDERS_SEND_MESSAGE;
+
+export interface IwsOrdersAction {
+  type: TwsOrders;
+  payload: unknown;
+}
