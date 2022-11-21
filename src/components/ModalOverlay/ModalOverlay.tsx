00abf03e5a162af9a2981b51e629f33270662b5b
Vadim Belousov <thevadiratti@yandex.ru>
2022-11-21 19:23:45 +0500

    refactor: ts типизация ModalOverlay

--- src/components/ModalOverlay/ModalOverlay.tsx
+++ src/components/ModalOverlay/ModalOverlay.tsx
@@ -1,15 +1,22 @@
+import { ReactNode } from 'react';
 import modalOverlayStyle from './ModalOverlay.module.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from '../../hooks/hooks';
 import { closeModalAction } from '../../services/actions/modal';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function ModalOverlay({ children, prePage, isPageType }) {
+type TProps = {
+  children: ReactNode;
+  prePage: string;
+  isPageType: boolean;
+};
+
+function ModalOverlay({ children, prePage, isPageType }: TProps) {
   const dispatch = useDispatch();
   const history = useHistory();
   const sendOrderRequest = useSelector((state) => state.order.isLoaded);
 
-  function closePopupOverlay(e) {
+  function closePopupOverlay(e: any) {
     if (!sendOrderRequest) {
       if (e.target.getAttribute('id') === 'overlay') {
         dispatch(closeModalAction());
