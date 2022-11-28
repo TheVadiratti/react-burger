03b8677ff2374db0565d47596e4ff018779266ce
Vadim Belousov <thevadiratti@yandex.ru>
2022-11-28 19:34:45 +0500

    fix: исправить ошибки типизации IngredientDetails

--- src/components/IngredientDetails/IngredientDetails.tsx
+++ src/components/IngredientDetails/IngredientDetails.tsx
@@ -15,31 +15,38 @@ function IngredientDetails() {
     return item._id === params.id;
   });
 
-  return (currentIngredient &&
+  return (
     <>
-      <div className={ingredientDetails.headingCnt}>
-        <h2 className={`text text_type_main-large ${isPageView && ingredientDetails.headingTypeGeneral}`}>Детали ингредиента:</h2>
-      </div>
-      <img className={ingredientDetails.image} src={currentIngredient.image} alt={currentIngredient.name}></img>
-      <span className="mt-4 text text_type_main-medium">{currentIngredient.name}</span>
-      <div className={`mt-8 mb-5 ${ingredientDetails.infoCnt}`}>
-        <div className={ingredientDetails.infoItem}>
-          <span className='text text_type_main-default text_color_inactive'>Калории,ккал</span>
-          <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.calories}</span>
-        </div>
-        <div className={`${ingredientDetails.infoItem}`}>
-          <span className='text text_type_main-default text_color_inactive'>Белки, г</span>
-          <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.proteins}</span>
-        </div>
-        <div className={`${ingredientDetails.infoItem}`}>
-          <span className='text text_type_main-default text_color_inactive'>Жиры, г</span>
-          <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.fat}</span>
-        </div>
-        <div className={`${ingredientDetails.infoItem}`}>
-          <span className='text text_type_main-default text_color_inactive'>Углеводы, г</span>
-          <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.carbohydrates}</span>
-        </div>
-      </div>
+      {currentIngredient ? (
+        <>
+          <div className={ingredientDetails.headingCnt}>
+            <h2 className={`text text_type_main-large ${isPageView && ingredientDetails.headingTypeGeneral}`}>Детали ингредиента:</h2>
+          </div>
+          <img className={ingredientDetails.image} src={currentIngredient.image} alt={currentIngredient.name}></img>
+          <span className="mt-4 text text_type_main-medium">{currentIngredient.name}</span>
+          <div className={`mt-8 mb-5 ${ingredientDetails.infoCnt}`}>
+            <div className={ingredientDetails.infoItem}>
+              <span className='text text_type_main-default text_color_inactive'>Калории,ккал</span>
+              <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.calories}</span>
+            </div>
+            <div className={`${ingredientDetails.infoItem}`}>
+              <span className='text text_type_main-default text_color_inactive'>Белки, г</span>
+              <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.proteins}</span>
+            </div>
+            <div className={`${ingredientDetails.infoItem}`}>
+              <span className='text text_type_main-default text_color_inactive'>Жиры, г</span>
+              <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.fat}</span>
+            </div>
+            <div className={`${ingredientDetails.infoItem}`}>
+              <span className='text text_type_main-default text_color_inactive'>Углеводы, г</span>
+              <span className='mt-2 text text_type_digits-default text_color_inactive'>{currentIngredient.carbohydrates}</span>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p className={`text text_type_main-medium ${ingredientDetails.errorName}`}>Ингредиент не найден</p>
+      )}
+
     </>
   )
 }
