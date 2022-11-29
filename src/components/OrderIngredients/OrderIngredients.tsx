9ec96d437a0c38073ce945cda8a0ce1f9c2dbd5f
Vadim Belousov <thevadiratti@yandex.ru>
2022-11-29 13:26:41 +0500

    fix: исправить ошибки типизации OrderIngredients

--- src/components/OrderIngredients/OrderIngredients.tsx
+++ src/components/OrderIngredients/OrderIngredients.tsx
@@ -6,35 +6,40 @@ type TProps = {
 };
 
 function OrderIngredients({ ingredientsArr }: TProps) {
-  console.log(ingredientsArr);
   const findIngredient = useFindIngredient();
-  return ingredientsArr.map((ingredientID, i) => {
-    if (i < 5) {
-      const currentIngredient = findIngredient(ingredientID);
-      if (currentIngredient) {
-        return (
-          <div className={orderIngredientsStyles.fakeBorder} style={{ zIndex: `${ingredientsArr.length - i}` }} key={i}>
-            <div className={orderIngredientsStyles.img} style={{ backgroundImage: `url(${currentIngredient.image})` }} key={i}></div>
-          </div>
-        )
-      }
-    }
-    else if (i === 5) {
-      const currentIngredient = findIngredient(ingredientID);
-      if (currentIngredient) {
-        const remains = String(ingredientsArr.length - 5);
-        return (
-          <div className={orderIngredientsStyles.fakeBorder} style={{ zIndex: `${ingredientsArr.length - i}` }} key={i}>
-            <div className={orderIngredientsStyles.img} style={{ backgroundImage: `url(${currentIngredient.image})` }} key={i}>
-              <div className={orderIngredientsStyles.imgOverlay}>
-                <span className='text text_type_main-default'>{'+' + remains}</span>
+
+  return (
+    <>
+      {
+      ingredientsArr.map((ingredientID, i) => {
+        if (i < 5) {
+          const currentIngredient = findIngredient(ingredientID);
+          if (currentIngredient) {
+            return (
+              <div className={orderIngredientsStyles.fakeBorder} style={{ zIndex: `${ingredientsArr.length - i}` }} key={i}>
+                <div className={orderIngredientsStyles.img} style={{ backgroundImage: `url(${currentIngredient.image})` }} key={i}></div>
               </div>
-            </div>
-          </div>
-        )
-      }
-    }
-  })
+            )
+          }
+        }
+        else if (i === 5) {
+          const currentIngredient = findIngredient(ingredientID);
+          if (currentIngredient) {
+            const remains = String(ingredientsArr.length - 5);
+            return (
+              <div className={orderIngredientsStyles.fakeBorder} style={{ zIndex: `${ingredientsArr.length - i}` }} key={i}>
+                <div className={orderIngredientsStyles.img} style={{ backgroundImage: `url(${currentIngredient.image})` }} key={i}>
+                  <div className={orderIngredientsStyles.imgOverlay}>
+                    <span className='text text_type_main-default'>{'+' + remains}</span>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        }
+      })}
+    </>
+  )
 }
 
 export default OrderIngredients;
